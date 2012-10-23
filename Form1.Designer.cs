b5650771ca813d4d1ecdb8952190c3c3a84bafd7
Partizanes <partizanes@bigmir.net>
2012-10-23 16:23:16 +0300

    [1]init commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Update
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Update));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(173, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Update
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(197, 46);
+            this.ControlBox = false;
+            this.Controls.Add(this.progressBar1);
+            this.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Update";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Утилита обновления";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Update_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Update : Form
+    {
+        public Update()
+        {
+            InitializeComponent();
+        }
+
+        private void Update_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void log_write(string str, string reason, string logname)
+        {
+            string EntryTime = DateTime.Now.ToLongTimeString();
+            string EntryDate = DateTime.Today.ToShortDateString();
+            string fileName = "log/" + EntryDate + "/" + logname + ".log";  //log + data +logname ? 
+
+            if (!Directory.Exists(Environment.CurrentDirectory + "/log/" + EntryDate + "/"))
+            {
+                Directory.CreateDirectory((Environment.CurrentDirectory + "/log/" + EntryDate + "/"));
+            }
+
+            try
+            {
+                StreamWriter sw = new StreamWriter(fileName, true, System.Text.Encoding.UTF8);
+                sw.WriteLine("[" + EntryDate + "][" + EntryTime + "][" + reason + "]" + " " + str);
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                log_write(ex.Message, "Exception", "Exception");
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            log_write("Выход из прогаммы обновления ", "INFO", "Update");
+            Application.Exit();
+        }
+
+    }
+}
